fe379952eacf31b1f55dc00c6f0c5242ddd2ce09
rina-ink <dominkovicmarina@gmail.com>
2026-06-15 00:41:29 +0200

    add local storage for entries

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import AddEntryModal from "./components/AddEntryModal";
 import EntryList from "./components/EntryList";
 import Header from "./components/Header";
 
+const STORAGE_KEY = "inner-compass-entries";
+
 const App = () => {
-  const [entries, setEntries] = useState([]);
+  const [entries, setEntries] = useState(() => {
+    const savedEntries = localStorage.getItem(STORAGE_KEY);
+
+    return savedEntries ? JSON.parse(savedEntries) : [];
+  });
+
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(entries));
+  }, [entries]);
 
   const addEntry = (newEntry) => {
     setEntries((prevEntries) => {
